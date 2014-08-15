b4b75325936d5626d7d6839a72c8d85d2a413ec7
Aditya Shailendra Deshpande <adid@plus.seas.upenn.edu>
2014-08-14 22:08:31 -0400

    Added Iterator to SLinked List

--- SLinkedList.h
+++ SLinkedList.h
@@ -1,3 +1,7 @@
+/*
+Implementation of a generic Singly Linked List in C++
+*/
+#pragma once
 #ifndef SLINKED_LIST_H
 #define SLINKED_LIST_H
 
@@ -6,6 +10,10 @@
 template< class T>
 class SLinkedList;
 
+
+/*
+Declaration of the Node
+*/
 template <class T>
 class Node{
 
@@ -20,6 +28,9 @@ class Node{
 	friend class SLinkedList<T>;
 };
 
+/*
+Declaration of the Singly Linked List
+*/
 template<class T>
 class SLinkedList{
 
@@ -29,8 +40,31 @@ class SLinkedList{
 	void reverseList(Node<T> *thead, Node<T> * ttail);
 
 	public:
+	class Iterator{
+		
+		private:
+		Node<T> * v;
+		Iterator(Node<T> * p){
+			v = p;
+		}
+
+		public:
+		T& operator*();
+		bool operator==(const Iterator& p)const;
+		bool operator!=(const Iterator& p)const;
+		Iterator& operator++();
+		
+		friend class SLinkedList<T>;
+	};
+
 	SLinkedList();
 	~SLinkedList();
+	SLinkedList(const SLinkedList<T>& s);
+
+	SLinkedList<T>& operator=(const SLinkedList<T>& s);
+
+	Iterator begin()const;
+	Iterator end()const;
 
 	void addToHead(const T& t);
 	void addToTail(const T& t);
@@ -48,10 +82,66 @@ class SLinkedList{
 	void display() const;
 };
 
+/*
+Singly Linked List constructor
+*/
 template<class T>
 SLinkedList<T>::SLinkedList():head(0), tail(0){
 }
 
+/*
+Singly Linked List copy constructor
+*/
+template<class T>
+SLinkedList<T>::SLinkedList(const SLinkedList<T>& s):head(0),tail(0){
+	
+	for (SLinkedList<T>::Iterator i = s.begin(); i != s.end(); ++i){
+		addToTail(*i);
+
+	}
+	std::cout << std::endl;
+}
+
+/*
+Singly linked list assignment operator
+*/
+template<class T>
+SLinkedList<T>& SLinkedList<T>::operator=(const SLinkedList<T>& s){
+	
+	if (this != &s){
+		while(!isEmpty()){
+			removeFromHead();
+		}
+
+		for (SLinkedList<T>::Iterator i = s.begin(); i != s.end(); ++i){
+			addToTail(*i);
+		}
+	}
+	return *this;
+}
+
+/*
+Returns an iterator pointing to the head of the list
+*/
+template<class T>
+typename SLinkedList<T>::Iterator SLinkedList<T>::begin() const{
+	return Iterator(head);
+}
+
+
+/*
+Returns an iterator pointing to the node netx to the tail. ie null
+*/
+template<class T>
+typename SLinkedList<T>::Iterator SLinkedList<T>::end() const{
+	return Iterator(tail->next);
+}
+
+
+/*
+Singly Linked List Destructor
+Removes all the nodes one by one.
+*/
 template<class T>
 SLinkedList<T>::~SLinkedList(){
 	while (!isEmpty()){
@@ -59,6 +149,10 @@ SLinkedList<T>::~SLinkedList(){
 	}
 }
 
+
+/*
+Adds the given element to the head of the linked list
+*/
 template<class T>
 void SLinkedList<T>::addToHead(const T& t){
 	if (head == 0){
@@ -70,8 +164,13 @@ void SLinkedList<T>::addToHead(const T& t){
 	}
 }
 
+
+/*
+Adds the given element to the tail of the linked list.
+*/
 template<class T>
 void SLinkedList<T>::addToTail(const T& t){
+	
 	if (tail == 0){
 		tail = new Node<T>(t);
 		head = tail;
@@ -84,9 +183,9 @@ void SLinkedList<T>::addToTail(const T& t){
 
 
 /*
-Assumes the List is not empty.
+Precondition: list should not be empty.
 isEmpty() check to be performed prior to call.
-Returns the element that was stored in the head.
+Removes the element from the head and returns it's value.
 */
 template<class T>
 T SLinkedList<T>::removeFromHead(){
@@ -105,9 +204,9 @@ T SLinkedList<T>::removeFromHead(){
 
 
 /*
-Assumes the List is not empty.
+Precondition: list should not be empty.
 isEmpty() check to be performed prior to the call.
-Returns the element that was stored in the tail.
+Removes the element from the tail and returns it's value.
 */
 template <class T>
 T SLinkedList<T>::removeFromTail(){
@@ -129,9 +228,11 @@ T SLinkedList<T>::removeFromTail(){
 	return val;
 }		
 
+
 /*
-Assumes the list is not empty.
+Precondition: list is not empty.
 isEmpty() check to be performed prior to the removal.
+Returns the minimum value present in the list.
 */
 template <class T>
 T SLinkedList<T>::getMin()const{
@@ -155,8 +256,9 @@ T SLinkedList<T>::getMin()const{
 
 
 /*
-Assumes the list is not empty.
+Precondition: List is not empty.
 isEmpty() check to be performed prior to the removal.
+Returns the maximum value present in the List.
 */
 template <class T>
 T SLinkedList<T>::getMax()const{
@@ -178,49 +280,50 @@ T SLinkedList<T>::getMax()const{
 }
 
 
-
-
+/*
+Removes all the elements equal to the value  specified in the argument.
+*/
 template<class T>
 void SLinkedList<T>::remove(const T& t){
 	if (!isEmpty()){
-		if (head == tail){
-			if (head->elem == t){
-				delete head;
-				head = 0;
-				tail = 0;
-			}
-		}
-		else{
-			if(head->elem == t){
+		
+		Node<T> * tmp = head;
+		Node<T> * prevTmp;
+		while (tmp != 0){
+			if (tmp->elem == t && tmp == head){
 				removeFromHead();
+				tmp = head;
 			}
-			else if (tail->elem == t){
+			else if (tmp->elem == t && tmp == tail){
 				removeFromTail();
+				tmp = tail->next;
+			}
+			else if (tmp->elem == t){
+				prevTmp->next = tmp->next;
+				delete tmp;
+				tmp = prevTmp->next;
 			}
 			else{
-				Node<T> *tmp = head->next;
-				Node<T> *tmpPrev = head;
-				
-				while (tmp != tail){
-					if (tmp->elem == t){
-						tmpPrev->next = tmp->next;
-						delete tmp;
-						return;
-					}
-					tmp = tmp->next;
-					tmpPrev = tmpPrev->next;
-				}
+				prevTmp=tmp;
+				tmp = tmp->next;
 			}
 		}
 	}
 }
-			
 
+			
+/*
+Returns true if the list is empty.
+*/
 template <class T>
 bool SLinkedList<T>::isEmpty() const{
 	return head == 0;
 }
 
+
+/*
+Returns true if the list contains the element specified in the argument
+*/
 template <class T>
 bool SLinkedList<T>::contains(const T& t) const{
 		
@@ -235,11 +338,18 @@ bool SLinkedList<T>::contains(const T& t) const{
 	return false;
 }
 
+
+/*
+Reverses the sequence of elements stored in the list.
+*/
 template<class T>
 void SLinkedList<T>::reverse(){
 	reverseList(head, tail);
 }
 
+/*
+Helper private method to reverse the list.
+*/
 template<class T>
 void SLinkedList<T>::reverseList(Node<T> * thead, Node<T> * ttail){
 	if (ttail == thead){
@@ -263,6 +373,9 @@ void SLinkedList<T>::reverseList(Node<T> * thead, Node<T> * ttail){
 	}
 }
 
+/*
+Display the elements stored in the list.
+*/
 template<class T>
 void SLinkedList<T>::display()const{
 	Node<T> * tmp = head;
@@ -272,6 +385,50 @@ void SLinkedList<T>::display()const{
 		tmp = tmp->next;
 	}
 	std::cout << std::endl;
+
 }
+
+/*
+Iterator methods
+*/
+
+/*
+Returns the value stored in the node that the iterator points to.
+*/
+template<class T>
+T& SLinkedList<T>::Iterator::operator*(){
+	return v->elem;
+}
+
+
+/*
+Returns true if the two iterators point to the same node.
+*/
+template<class T>
+bool SLinkedList<T>::Iterator::operator==(const SLinkedList<T>::Iterator& p)const{
+	return v == p.v;
+}
+
+
+/*
+Returns true if the two iterators point to different nodes.
+*/
+template<class T>
+bool SLinkedList<T>::Iterator::operator!=(const SLinkedList<T>::Iterator& p) const{
+	return v != p.v;
+}
+
+
+/*
+Iterator increment operator
+*/
+template <class T>
+typename SLinkedList<T>::Iterator& SLinkedList<T>::Iterator::operator++(){
+
+	v = v->next;
+	return *this;
+	
+}
+
 
 #endif
